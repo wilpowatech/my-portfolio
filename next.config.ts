c65de35961a64e03167fb3ff0a32c5117fbcf6a4
wilpowatech <williamkhoko@gmail.com>
2026-03-19 21:16:52 +0100

    20th mar changes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/my-portfolio',
+  assetPrefix: '/my-portfolio',
   images: {
     unoptimized: true,
     remotePatterns: [
